01f27497bc1eabbfa72568237ddd9fadd7ca7e5d
wjz2001 <wjz2001@users.noreply.github.com>
2024-06-07 10:37:58 +0800

    适配bgo 2.86

--- app/src/main/java/com/autostart/bgo/MainActivity.java
+++ app/src/main/java/com/autostart/bgo/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        ComponentName cn = new ComponentName("com.bilibili.fatego", "com.unity3d.player.EmptyClass");
+        ComponentName cn = new ComponentName("com.bilibili.fatego", "com.bilibili.fatego.EmptyClass");
         intent.setComponent(cn);
         startActivity(intent);
     }
