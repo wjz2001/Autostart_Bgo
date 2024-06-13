56d5e3625c6df8b181cf7a2e159bf793afb6e724
wjz2001 <wjz2001@users.noreply.github.com>
2024-06-13 19:57:03 +0800

    bug修复

--- app/src/main/java/com/autostart/bgo/MainActivity.java
+++ app/src/main/java/com/autostart/bgo/MainActivity.java
@@ -1,11 +1,9 @@
 package com.autostart.bgo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.content.pm.PackageManager;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -17,19 +15,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, PushService.class);
         startService(intent);
-        setTitle("MainActivity");
-        launchBgo();
+        setTitle("启动命运·冠位指定");
     }
 
-    private void launchBgo() {
+    @Override
+    protected void onStart() {
+        super.onStart();
         PackageManager packageManager = getPackageManager();
         Intent launchIntent = packageManager.getLaunchIntentForPackage(PACKAGE_NAME);
-
-        if (launchIntent != null) {
-            startActivity(launchIntent);
-        } else {
-            Toast.makeText(this, "命运·冠位指定未安装", Toast.LENGTH_SHORT).show();
-        }
+        launchIntent.setAction(Intent.ACTION_MAIN);
+        launchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        startActivity(launchIntent);
     }
 }
 
